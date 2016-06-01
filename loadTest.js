8ec01c5d0cbcbc6b327162cd38ad95c7234dcc25
RAFAEL REIS FERREIRA <rafaelrferreira@gmail.com>
2016-06-01 00:52:20 -0300

    alterações de cargas dos testes

--- loadTest.js
+++ loadTest.js
@@ -5,20 +5,20 @@ var receiver = "get@getmush.com.br";
 console.log("Iniciando teste de carga:");
 
 //TESTE LEVE
-mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "www.google.com");
-mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "https://www.tsestoque.com.br");
-mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "http://www.universidadeestoque.com.br");
+// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "www.google.com");
+// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "https://www.tsestoque.com.br");
+// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "http://www.universidadeestoque.com.br");
 
 //TESTE MÉDIO
-// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "www.google.com");
-// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "https://www.tsestoque.com.br");
-// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "http://www.yahoo.com");
-// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "www.google.com");
-// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "https://www.tsestoque.com.br");
-// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "http://www.yahoo.com");
-// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "www.google.com");
-// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "https://www.tsestoque.com.br");
-// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "http://www.yahoo.com");
+// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "www.theverge.com");
+// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "toque.com.br");
+mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "http://www.sadasdasd.com");
+// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "www..com");
+// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "http://projteste02.azurewebsites.net/HtmlPage1.html");
+// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "http://spazioacqua.pe.hu");
+// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "http://www.bling.com");
+// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "http://google.comhttp://google.com");
+// mailSender.sendEmailLoadTest("loadtest..." ,sender, receiver, "https://www.estoquevivo.com.br");
 
 //TESTE PESADO
 
