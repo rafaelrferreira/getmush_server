9cc91a0eb1ad7e1a27778d420e87840a740e709f
RAFAEL REIS FERREIRA <rafaelrferreira@gmail.com>
2016-12-31 16:19:41 -0200

    Teste GET POST ajax url

--- BrowserRequestHandler.js
+++ BrowserRequestHandler.js
@@ -2,7 +2,17 @@ var express = require('express');
 
 app = express();
 app.get('/', function(req, res){
-    res.send('Ola Mundo');
+    res.send('GetMushe Browser URL Analyser ON.');
+    console.log('#root request');
+});
+app.get('/GetUrlImage/', function(req, res){
+	var url = req.query.url; 
+    res.send('Retrieve URL Image Base64: ' + url);
+	//res.send('Retrieve URL Image Base64 using params:' + req.params.url);    
+
+    //Processa imagem da URL com Phantom e retorna o Base64 
+
+    console.log('#GetUrlImage request');
 });
 
 var server = app.listen(3000);
