746f01ac90d3c2fe5e1561975fbbf6b9267fac2c
RAFAEL REIS FERREIRA <rafaelrferreira@gmail.com>
2017-01-02 21:39:19 -0200

    Teste de passagem de parametro do PROMISE para o GET do ajax
    ainda não funciona com promise

--- BrowserRequestHandler.js
+++ BrowserRequestHandler.js
@@ -1,18 +1,29 @@
 var express = require('express');
+var phManager = require('./Phantom.js');
 
 app = express();
+
 app.get('/', function(req, res){
     res.send('GetMushe Browser URL Analyser ON.');
     console.log('#root request');
 });
+
 app.get('/GetUrlImage/', function(req, res){
 	var url = req.query.url; 
-    res.send('Retrieve URL Image Base64: ' + url);
+	console.log('Url to process: '+url);
+	//urlResp = JSON.parse(req.params);
+	//console.log(req.params.url);
+
+	//res.header('Content-type','application/json');
+	//res.header('Charset','utf8');
+
 	//res.send('Retrieve URL Image Base64 using params:' + req.params.url);    
 
     //Processa imagem da URL com Phantom e retorna o Base64 
-
-    console.log('#GetUrlImage request');
+    phManager.mushMushProcessUrl(url);
+    console.log('base64: ' + phManager.respB64());
+    res.send(JSON.stringify(url));
+    console.log('Process completed.');
 });
 
 var server = app.listen(3000);
--- Phantom.js
+++ Phantom.js
@@ -5,11 +5,12 @@ var queueManager = require("./Queue.js");
 var mailSender = require("./MailSender.js");
 var eventController = require("./EventController.js");
 
+var imgB64 = null;
 var sitepage = null;
 var phInstance = null;
 title = 'default', msg = 'Hold the door! - Hodor';
 
-//Processa a URL de entrada
+//Processa a URL de entrada e envia para um email
 exports.mushMushProcess = function (url, emailTo) {
     //Execução do Phantom
     phantom.create()
@@ -70,10 +71,75 @@ exports.mushMushProcess = function (url, emailTo) {
         });
 }
 
+//Processa a URL de entrada
+exports.mushMushProcessUrl = function  (url) {
+    //Execução do Phantom
+    phantom.create()
+        .then(instance => {
+            phInstance = instance;
+            return instance.createPage();
+        })
+        .then(page => {
+            page.property('viewportSize', { width: 800, height: 600 });
+
+            //RESOURCE RECEIVED  
+            page.property('onResourceReceived', function (res) {
+                //console.log(res.id + ' ' + res.status + ' - ' + res.url);
+                if (!res.stage || res.stage === 'end') {
+                    count -= 1;
+                    //console.log(res.id + ' ' + res.status + ' - ' + res.url);
+                    if (count === 0) {
+                        //renderTimeout = setTimeout(doRender, resourceWait);
+                    }
+                }
+            }).then(function () {
+                console.log("success set onResourceReceived".green);
+            });
+
+            sitepage = page;
+            return sitepage.open(url);
+        })
+        .then(status => {
+            if (status !== "success") {
+                console.log("PHANTOM -----------------------> " + url);
+                msg = 'Não foi possível carregar a URL. Status: ' + status;
+                console.log(colors.red(msg));
+                
+                phInstance.exit();
+                
+            } else {
+                var b64 = null;
+                //Gera a imagem no formato Base64
+                b64 = sitepage.renderBase64('PNG');
+                return b64;
+            }
+        })
+        .then(b64 => {
+            console.log(colors.green('Renderização da url finalizada.'));
+            return b64;
+
+            cleanup();
+        })
+        .then(function(result) {
+            //exports.respB64 = function(req, res){
+                //console.log('Hello world');
+                //res.send('Hello world');
+            //};
+        })
+        .catch(error => {
+            console.log(colors.red(error));
+            phInstance.exit();
+        });
+}
+
+//exports.respB64 = imgB64;
+//exports.respB64 = mushMushProcessUrl("https://www.google.com.br");
+
 exports.RegisterEvent = function (){
     eventController.AddListenerToEvent("OnPop", ExecuteRequestAndRemoveListener);
     console.log("#Phantom listening, WAITING FOR NEW REQUESTS: ".silly);
 }
+
 
 //Fecha e finaliza as variáveis do Phantom
 function cleanup() {
